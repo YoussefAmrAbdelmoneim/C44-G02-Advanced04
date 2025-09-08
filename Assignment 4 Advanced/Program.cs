3c1409d8763d74f711e1af2bc10b2c3eed919262
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-09-08 12:52:37 +0300

    Add project files.

---
+++ Assignment 4 Advanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_4_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
