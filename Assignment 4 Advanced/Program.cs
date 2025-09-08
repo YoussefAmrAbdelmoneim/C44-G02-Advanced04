f58e06dbf76767b976041c016ef4c0a7df5dffc4
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-09-08 13:05:29 +0300

    Q1

--- Assignment 4 Advanced/Program.cs
+++ Assignment 4 Advanced/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Assignment_4_Advanced
+﻿using System.Collections;
+
+namespace Assignment_4_Advanced
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //int[] numbers = { 1, 2, 2, 3, 1, 4, 5, 2, 3, 1, 5, 5 };
+            //Hashtable hashtable = new Hashtable();
+            //foreach (int item in numbers)
+            //{
+            //    if (hashtable.ContainsKey(item))
+            //    {
+            //        hashtable[item] = (int?)hashtable[item] + 1;
+            //    }
+            //    else
+            //    {
+            //        hashtable[item] = 1;
+            //    }
+            //}
+            //foreach (DictionaryEntry item in hashtable)
+            //{
+            //    Console.WriteLine($"Element {item.Key}: {item.Value} times");
+            //}
+            #endregion
         }
     }
 }
